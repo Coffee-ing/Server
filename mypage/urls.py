85b2c990aa263a4356d76b10218ea708fd4d253c
yerinlee <dpfls0922@naver.com>
2023-11-06 01:30:20 +0900

    [Chore] 신청한 모임 조회 및 삭제 엔드포인트 변경

--- mypage/urls.py
+++ mypage/urls.py
@@ -6,8 +6,8 @@ from .views import *
 app_name = "mypage"
 
 urlpatterns = [
-    path('myclub/', get_my_apply, name='myapply'),
-    path('myclub/<int:post_id>/', delete_my_apply, name='delete-myapply'),
+    path('myapply/', get_my_apply, name='myapply'),
+    path('myapply/<int:post_id>/', delete_my_apply, name='delete-myapply'),
     path('mylike/', get_my_like, name='myapply'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

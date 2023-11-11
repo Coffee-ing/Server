8750de5a7c910d477b6e83acf31e242e6bf3cc23
yerinlee <dpfls0922@naver.com>
2023-11-11 15:29:22 +0900

    [Chore] 신청한 모임 조회 시 모든 필드 정보가 포함되도록 수정

--- mypage/serializers.py
+++ mypage/serializers.py
@@ -6,14 +6,11 @@ from main.serializers import *
 
 class MyClubSerializer(ModelSerializer):
     club_id = serializers.CharField(source='club.id', read_only=True)
-    title = serializers.CharField(source='club.title', read_only=True)
-    district = serializers.CharField(source='club.district', read_only=True)
-    meet_time = serializers.CharField(source='club.meet_time', read_only=True)
-    organizer = serializers.CharField(source='club.organizer', read_only=True)
+    club = ClubSerializer()
 
     class Meta:
         model = MyClub
-        fields = ['id', 'club_id', 'title', 'district', 'meet_time', 'organizer']
+        fields = ['club_id', 'club']
 
 
 class MyLikeSerializer(ModelSerializer):
